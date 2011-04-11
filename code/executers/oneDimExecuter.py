51ba2405b48d34a5af40472cfb14582a04d563d2
Caio Valentim <kakaio9@gmail.com>
2011-04-10 22:08:56 -0300

    Now the user can write "random x" a the input sequence will be a random sequence of x elements.

--- code/executers/oneDimExecuter.py
+++ code/executers/oneDimExecuter.py
@@ -1,9 +1,14 @@
 import io
 import sys
+import random
 from executer import Executer
 
 class OneDimExecuter(Executer):
 	def _parse_line(self, line):
+		if line.startswith("random"):
+			n = int(line.split(" ")[1])
+			res = [random.randint(0, 10000) for x in xrange(n)]
+			return res
 		line = line.split(',')
 		res = [int(x) for x in line]
 		return res
--- code/executers/twoDimExecuter.py
+++ code/executers/twoDimExecuter.py
@@ -1,10 +1,22 @@
 import io
 import sys
+import random
 
 from executer import Executer
 
 class TwoDimExecuter(Executer):
 	def _parse_line(self, line):
+		if line.startswith("random"):
+			if len(line.split(" ")) <= 1:
+				print sys.stderr, "Must include the amount of \
+								  number in the random sequence."
+				return None
+			try:
+				n = int(line.split(" ")[1])
+			except ValueError:
+				print sys.stderr, "Format is random <integer>."
+				return None
+			return [random.randint(1, 10000) for x in xrange(n)]
 		line = line.replace('(', ' ').replace(')', ' ')
 		line = line.split(',')
 		if len(line)%2 != 0 or len(line) < 2:
