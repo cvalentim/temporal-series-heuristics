179d2655dcfe6aecfb91e16bc92a891547c64333
Caio Valentim <kakaio9@gmail.com>
2011-05-10 22:19:40 -0300

    Modifing the rmq's nlog n implementation to respond queries with the minimum element index and not the value. Also, updated the test file to reflect it.

--- code/heuristics/rmq.py
+++ code/heuristics/rmq.py
@@ -8,16 +8,22 @@ class RMQ:
 		n = len(A)
 		k = int(math.log(n, 2))
 		self.PD = [[-1]*(k + 1) for x in xrange(n)]
+		self.ID = [[-1]*(k + 1) for x in xrange(n)]
 		for i in xrange(n):
 			self.PD[i][0] = A[i]
+			self.ID[i][0] = i
 		for delta in xrange(1, k + 1):
 			for i in xrange(n):
 				self.PD[i][delta] = self.PD[i][delta - 1]
+				self.ID[i][delta] = self.ID[i][delta - 1]
 				if i + 2**(delta - 1) < n:
-					self.PD[i][delta] = min(self.PD[i][delta], 
-										self.PD[i + 2**(delta - 1)][delta - 1])
+					if self.PD[i][delta] > self.PD[i + 2**(delta-1)][delta-1]:
+						self.PD[i][delta] = self.PD[i + 2**(delta-1)][delta-1]
+						self.ID[i][delta] = self.ID[i + 2**(delta-1)][delta-1]
 
 	def query(self, i, j):
 		k = int(math.log(j - i + 1, 2))
-		return min(self.PD[i][k], 
-					self.PD[j - 2**k + 1][k])
+		if self.PD[i][k] <= self.PD[j - 2**k + 1][k]:
+			return self.ID[i][k]
+		else:
+			return self.ID[j - 2**k + 1][k]
--- code/tests/test_rmq.py
+++ code/tests/test_rmq.py
@@ -10,30 +10,30 @@ class TestRMQ(unittest.TestCase):
 	def test_one_sequence(self):
 		self.rmq.preprocess([2])
 		# testando se ha lixo entre pesquisas
-		self.assertEqual(self.rmq.query(0, 0), 2)	
+		self.assertEqual(self.rmq.query(0, 0), 0)	
 
 	def test_two_sequence(self):
 		self.rmq.preprocess([3, 1])
 		self.assertEqual(self.rmq.query(0, 1), 1)
-		self.assertEqual(self.rmq.query(0, 0), 3)
+		self.assertEqual(self.rmq.query(0, 0), 0)
 		self.assertEqual(self.rmq.query(1, 1), 1)
 
 	def test_three_sequence(self):
 		self.rmq.preprocess([1, 3, 2])
-		self.assertEqual(self.rmq.query(0, 2), 1)
-		self.assertEqual(self.rmq.query(0, 1), 1)
-		self.assertEqual(self.rmq.query(1, 1), 3)
+		self.assertEqual(self.rmq.query(0, 2), 0)
+		self.assertEqual(self.rmq.query(0, 1), 0)
+		self.assertEqual(self.rmq.query(1, 1), 1)
 		self.assertEqual(self.rmq.query(1, 2), 2)
 		self.assertEqual(self.rmq.query(2, 2), 2)
 
 	def test_three_sequence_again(self):
 		self.rmq.preprocess([10, 2, 7])
-		self.assertEqual(self.rmq.query(0, 2), 2)
-		self.assertEqual(self.rmq.query(0, 1), 2)
-		self.assertEqual(self.rmq.query(0, 0), 10)
-		self.assertEqual(self.rmq.query(1, 2), 2)
-		self.assertEqual(self.rmq.query(1, 1), 2)
-		self.assertEqual(self.rmq.query(2, 2), 7)
+		self.assertEqual(self.rmq.query(0, 2), 1)
+		self.assertEqual(self.rmq.query(0, 1), 1)
+		self.assertEqual(self.rmq.query(0, 0), 0)
+		self.assertEqual(self.rmq.query(1, 2), 1)
+		self.assertEqual(self.rmq.query(1, 1), 1)
+		self.assertEqual(self.rmq.query(2, 2), 2)
 
 	def test_random_sequence(self):
 		import random
@@ -45,9 +45,12 @@ class TestRMQ(unittest.TestCase):
 			self.rmq.preprocess(A)
 			for i in xrange(n):
 				smaller = A[i]
+				smaller_id = i
 				for j in xrange(i, n):
-					smaller = min(smaller, A[j])
-					self.assertEqual(self.rmq.query(i, j), smaller)
+					if smaller >  A[j]:
+						smaller = A[j]	
+						smaller_id = j
+					self.assertEqual(self.rmq.query(i, j), smaller_id)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestRMQ)
 unittest.TextTestRunner(verbosity = 2).run(suite)
