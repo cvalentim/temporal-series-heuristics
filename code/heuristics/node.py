9d263d8c23093bb44201b1f7c5bd0f4d2f431f38
Caio Valentim <kakaio9@gmail.com>
2011-05-17 20:54:59 -0300

    Adding a new type of node. Binary node is suitable for binary trees.

--- code/heuristics/node.py
+++ code/heuristics/node.py
@@ -9,3 +9,33 @@ class Node:
 				self._id == other._id and self.children == other.children:
 			return True
 		return False
+
+class BinaryNode:
+	def __init__(self, value = None, _id = None, left = None, right = None, parent = None):
+		self.value = value
+		self._id = _id
+		self.left = left
+		self.right = right
+		self.parent = parent
+
+	def __lt__(self, other):
+		if self.value == other.value:
+			return self._id < other._id
+		return self.value < other.value
+
+	def __gt__(self, other):
+		if self.value == other.value:
+			return self._id > other._id
+		return self.value > other.value
+
+	def __eq__(self, other):
+		return self._id == other._id
+	
+	@property
+	def children(self):
+		res = []
+		if self.left:
+			res += [self.left]
+		if self.right:
+			res += [self.right]
+		return res
